c099cb63b2d9d61b9d474ecfd6233812392ff509
ethan <ethanorzethan@gmail.com>
2026-09-14 17:51:38 +0800

    feat:语言和系统风格设置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react'
 import { NavLink, Outlet } from 'react-router'
 import './App.css'
+import { useAppDispatch, useAppSelector } from './store/hooks'
+import { toggleSysLanguage, toggleSysStyle } from './store/slices/sysSetting'
 
 function App() {
+  const dispatch = useAppDispatch()
+  const { sysStyle, sysLanguage } = useAppSelector((state) => state.sysSetting)
+
+  useEffect(() => {
+    document.documentElement.dataset.theme = sysStyle
+    document.documentElement.lang = sysLanguage
+  }, [sysStyle, sysLanguage])
+
   return (
     <>
       <nav className="app-nav">
@@ -11,6 +22,20 @@ function App() {
         <NavLink to="/about">About</NavLink>
         <NavLink to="/add-user">Add User</NavLink>
         <NavLink to="/users">Users</NavLink>
+        <button
+          type="button"
+          className="app-setting"
+          onClick={() => dispatch(toggleSysStyle())}
+        >
+          {sysStyle === 'light' ? '暗色' : '亮色'}
+        </button>
+        <button
+          type="button"
+          className="app-setting"
+          onClick={() => dispatch(toggleSysLanguage())}
+        >
+          {sysLanguage === 'zh-CN' ? '中文' : 'English'}
+        </button>
       </nav>
       <Outlet />
     </>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import sysSettingReducer from './slices/sysSetting'
 import userReducer from './slices/userSlice'
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    sysSetting: sysSettingReducer,
   },
 })
 
